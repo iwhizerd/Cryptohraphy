c2985cf350804a43aeb5d2cc0f0cae179f967ffd
Toni <toni.benn.g@gmail.com>
2016-11-30 17:21:53 +0100

    Provant coses amb "tp" = transposition primera  cypher fet v

--- Transposition.java
+++ Transposition.java
@@ -4,32 +4,32 @@ public class Transposition {
 
         StringBuilder sb = new StringBuilder();
 
-        int columnes = (s.length() / dim);
+        int columnes = (int) (Math.ceil(s.length() / (double) dim));
+        //int columnes = (s.length() / dim);
 
-        if (columnes % dim == 1) {
-            columnes += 1;
-        }
+        //if (columnes % dim != 0) {
+        //   columnes += 1;
+        // }
 
         char arr [][] = new char [columnes][dim];
 
         int posicio = 0;
 
-        for (int x = 0; x < arr[0].length; x++) {
-            for (int y = 0; y < arr.length; y++) {
-                if (posicio < s.length()) {
-                    arr[y][x] = '*';
-                    posicio++;
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = 0; y < arr[0].length; y++) {
+                if (posicio == s.length()) {
+                    arr[x][y] = '*';
+                    continue;
                 }
-                arr [y][x] = s.charAt(posicio);
-                posicio ++;
-
-
+                arr[x][y] = s.charAt(posicio);
+                posicio++;
             }
         }
-
         for (int a = 0; a < arr[0].length; a++) {
-            for (int b = 0; b < arr.length; b++) {
-                sb.append(arr[b][a]);
+            for (char[] anArr : arr) {
+                if (anArr[a] != '*') {
+                    sb.append(anArr[a]);
+                }
             }
         }
         return sb.toString();
